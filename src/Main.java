f7964a10630d9d6628578a760f8fa22fd0883f95
Jasmien <jasmien.pajiji9@gmail.com>
2025-12-05 03:55:34 +0800

    new gui

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,44 @@
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumnModel;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, FontFormatException {
         // font
-        Font headerFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Bogista-DEMO.otf")).deriveFont(24f);
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        ge.registerFont(headerFont);
-
-        Font subtitleFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Akwe-Pro-Bold.otf")).deriveFont(14f);
+        Font headerFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/SFPRODISPLAYBOLD.OTF"));
+        Font subtitleFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/SFPRODISPLAYREGULAR.OTF"));
 
         // frame
         JFrame frame = new JFrame();
         frame.setTitle("Coffee Culture Point of Sale System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(Color.decode("#FEFCEF"));
+        frame.getContentPane().setBackground(Color.decode("#FFFFFF"));
         frame.setResizable(false);
-        frame.setSize(600, 500);
+        frame.setSize(767, 491);
         frame.setLayout(null);
         frame.setLocationRelativeTo(null);
 
         // POS cafè name
         JLabel title = new JLabel("Coffee Culture");
         title.setFont(headerFont.deriveFont(42f));
-        title.setForeground(Color.decode("#C13631"));
+        title.setForeground(Color.decode("#000000"));
+        title.setBackground(Color.decode("#FFFFFF"));
+        title.setOpaque(true);
         title.setHorizontalAlignment(JLabel.CENTER);
-        title.setBounds(0, 10, 600, 60);
+        title.setBounds(0, 0, 767, 60);
+        title.setBorder(new MatteBorder(0,0,2,0, Color.decode("#474747")));
 
         // product panel
-        JPanel productPanel = new JPanel(new GridLayout(3, 2, 8, 8));
-        productPanel.setBounds(0, 70, 335, 393);
-        productPanel.setBackground(Color.decode("#C13631"));
+        JPanel productPanel = new JPanel(new GridLayout(2, 3, 8, 8));
+        productPanel.setBounds(0, 60, 504, 395);
+        productPanel.setBackground(Color.decode("#FFFFFF"));
         productPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
 
         // product buttons inside product panel
@@ -50,15 +53,16 @@ public class Main {
 
         // cart display
         JPanel cart = new JPanel();
-        cart.setBounds(335, 70, 251, 264);
-        cart.setBackground(Color.decode("#C13631"));
+        cart.setBounds(504, 60, 251, 264);
+        cart.setBackground(Color.decode("#FFFFFF"));
+        cart.setBorder(new MatteBorder(0,1,1,0, Color.decode("#474747")));
         cart.setLayout(null);
 
         JLabel cartLabel = new JLabel("Current Order");
         cartLabel.setFont(headerFont.deriveFont(24f));
-        cartLabel.setForeground(Color.decode("#FEFCEF"));
+        cartLabel.setForeground(Color.decode("#000000"));
         cartLabel.setHorizontalAlignment(JLabel.LEFT);
-        cartLabel.setBounds(0, 5, 251, 30);
+        cartLabel.setBounds(10, 3, 251, 30);
 
         // cart table
         String[] columnNames = {"ITEM", "QTY", "PRICE"};
@@ -71,53 +75,60 @@ public class Main {
         };
 
         JTable cartTable = new JTable(model);
-        cartTable.setBounds(0, 54, 241, 96);
-        cartTable.setBackground(Color.decode("#C13631"));
-        cartTable.setForeground(Color.decode("#FEFCEF"));
+        cartTable.setBounds(10, 54, 231, 96);
+        cartTable.setBackground(Color.decode("#FFFFFF"));
+        cartTable.setForeground(Color.decode("#000000"));
         cartTable.setFont(subtitleFont.deriveFont(12f));
         cartTable.setShowGrid(true);
-        cartTable.setGridColor(Color.decode("#FEFCEF"));
-        cartTable.setBorder(BorderFactory.createLineBorder(Color.decode("#FEFCEF")));
+        cartTable.setGridColor(Color.decode("#474747"));
+        cartTable.setBorder(BorderFactory.createLineBorder(Color.decode("#474747")));
 
         TableColumnModel columnModel = cartTable.getColumnModel();
-        columnModel.getColumn(0).setPreferredWidth(140);
+        columnModel.getColumn(0).setPreferredWidth(135);
         columnModel.getColumn(1).setPreferredWidth(41);
-        columnModel.getColumn(2).setPreferredWidth(60);
+        columnModel.getColumn(2).setPreferredWidth(55);
 
         JTableHeader header = cartTable.getTableHeader();
-        header.setBounds(0, 35, 241, 20);
-        header.setBackground(Color.decode("#FEFCEF"));
-        header.setForeground(Color.decode("#C13631"));
-        header.setFont(subtitleFont.deriveFont(14f));
+        header.setBounds(10, 35, 231, 20);
+        header.setBackground(Color.decode("#FFFFFF"));
+        header.setForeground(Color.decode("#000000"));
+        header.setFont(headerFont.deriveFont(14f));
+        header.setBorder(BorderFactory.createLineBorder(Color.decode("#474747")));
 
         // total display
         JPanel display = new JPanel(new BorderLayout());
-        display.setBackground(Color.decode("#FEFCEF"));
-        display.setBounds(338, 336, 251, 122);
+        display.setBackground(Color.decode("#FFFFFF"));
+        display.setBounds(504, 322 , 251, 132);
+        display.setBorder(new MatteBorder(1,1,0,0, Color.decode("#474747")));
+        display.setVisible(true);
 
         JPanel topDisplay = new JPanel();
         topDisplay.setLayout(new BoxLayout(topDisplay, BoxLayout.Y_AXIS));
-        topDisplay.setBackground(Color.decode("#FEFCEF"));
+        topDisplay.setBackground(Color.decode("#FFFFFF"));
+        topDisplay.setBorder(new MatteBorder(1,0,0,0, Color.decode("#474747")));
 
         totalDisplay totals = new totalDisplay();
         totals.setCartTable(cartTable);
 
         JLabel subtotalLabel = new JLabel("");
         subtotalLabel.setFont(subtitleFont.deriveFont(20f));
-        subtotalLabel.setForeground(Color.decode("#C13631"));
+        subtotalLabel.setForeground(Color.decode("#000000"));
+        subtotalLabel.setBorder(BorderFactory.createEmptyBorder(5, 10, 0, 0));
 
         JLabel taxLabel = new JLabel("");
         taxLabel.setFont(subtitleFont.deriveFont(20f));
-        taxLabel.setForeground(Color.decode("#C13631"));
+        taxLabel.setForeground(Color.decode("#000000"));
+        taxLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
 
         JLabel totalLabel = new JLabel("");
-        totalLabel.setFont(subtitleFont.deriveFont(28f));
-        totalLabel.setForeground(Color.decode("#C13631"));
+        totalLabel.setFont(headerFont.deriveFont(28f));
+        totalLabel.setForeground(Color.decode("#000000"));
+        totalLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 5, 0));
 
         // wire product buttons to cart
         for (Product product : products) {
             ImageIcon rawIcon = product.getImage();
-            Image scaledImage = rawIcon.getImage().getScaledInstance(170, 170, Image.SCALE_SMOOTH);
+            Image scaledImage = rawIcon.getImage().getScaledInstance(5000, 5000, Image.SCALE_SMOOTH);
             ImageIcon scaledIcon = new ImageIcon(scaledImage);
 
             ProductButton prodButton = new ProductButton(
@@ -125,6 +136,9 @@ public class Main {
                     product.getPrice(),
                     scaledIcon.getImage()
             );
+
+            prodButton.setBorderPainted(true);
+            prodButton.setBorder(BorderFactory.createLineBorder(Color.decode("#000000"), 1));
 
             // add to cart when clicked
             prodButton.addActionListener(_ -> {
@@ -165,12 +179,14 @@ public class Main {
 
         // button to decrease quantity
         JButton decreaseButton = new JButton("Decrease Qty");
-        decreaseButton.setBounds(0, 166, 241, 25);
-        decreaseButton.setFont(subtitleFont.deriveFont(14f));
-        decreaseButton.setBackground(Color.decode("#FEFCEF"));
-        decreaseButton.setForeground(Color.decode("#C13631"));
+        decreaseButton.setBounds(10, 166, 231, 25);
+        decreaseButton.setFont(headerFont.deriveFont(14f));
+        decreaseButton.setBackground(Color.decode("#FFFFFF"));
+        decreaseButton.setForeground(Color.decode("#000000"));
         decreaseButton.setMargin(new Insets(0, 0, 0, 0));
         decreaseButton.setFocusPainted(false);
+        decreaseButton.setBorderPainted(true);
+        decreaseButton.setBorder(BorderFactory.createLineBorder(Color.decode("#000000"), 1));
 
         decreaseButton.addActionListener(_ -> {
             int selectedRow = cartTable.getSelectedRow();
@@ -181,51 +197,71 @@ public class Main {
                 } else {
                     model.removeRow(selectedRow); // remove row if quantity = 1
                 }
-
-                // update totals
-                subtotalLabel.setText("Subtotal: " + String.format("%.2f", totals.calculateSubtotal()));
-                taxLabel.setText("Tax: " + String.format("%.2f", totals.calculateTax()));
-                totalLabel.setText("TOTAL: " + String.format("%.2f", totals.calculateTotal()));
             } else {
                 JOptionPane.showMessageDialog(frame, "Please select an item to decrease.");
             }
         });
 
+        // button to clear cart
         JButton clearButton = new JButton("Clear Cart");
-        clearButton.setBounds(0, 198, 241, 25);
-        clearButton.setFont(subtitleFont.deriveFont(14f));
-        clearButton.setBackground(Color.decode("#FEFCEF"));
-        clearButton.setForeground(Color.decode("#C13631"));
+        clearButton.setBounds(10, 198, 231, 25);
+        clearButton.setFont(headerFont.deriveFont(14f));
+        clearButton.setBackground(Color.decode("#FFFFFF"));
+        clearButton.setForeground(Color.decode("#000000"));
         clearButton.setMargin(new Insets(0, 0, 0, 0));
         clearButton.setFocusPainted(false);
+        clearButton.setBorderPainted(true);
+        clearButton.setBorder(BorderFactory.createLineBorder(Color.decode("#000000"), 1));
 
         clearButton.addActionListener(_ -> {
             model.setRowCount(0);
-            display.setVisible(false);
+            subtotalLabel.setText("");
+            taxLabel.setText("");
+            totalLabel.setText("");
         });
 
         // button to check out cart
         JButton checkoutButton = new JButton("Check Out");
-        checkoutButton.setBounds(0, 230, 241, 25);
-        checkoutButton.setFont(subtitleFont.deriveFont(14f));
-        checkoutButton.setBackground(Color.decode("#FEFCEF"));
-        checkoutButton.setForeground(Color.decode("#C13631"));
+        checkoutButton.setBounds(10, 230, 231, 25);
+        checkoutButton.setFont(headerFont.deriveFont(14f));
+        checkoutButton.setBackground(Color.decode("#FFFFFF"));
+        checkoutButton.setForeground(Color.decode("#00000"));
         checkoutButton.setMargin(new Insets(0, 0, 0, 0));
         checkoutButton.setFocusPainted(false);
+        checkoutButton.setBorderPainted(true);
+        checkoutButton.setBorder(BorderFactory.createLineBorder(Color.decode("#000000"), 1));
 
         checkoutButton.addActionListener(_ -> {
             subtotalLabel.setText("Subtotal: " + String.format("%.2f", totals.calculateSubtotal()));
             taxLabel.setText("Tax: " + String.format("%.2f", totals.calculateTax()));
             totalLabel.setText("Total: " + String.format("%.2f", totals.calculateTotal()));
-
-            display.setVisible(true);
         });
+
+        MouseAdapter hoverEffect = new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                JButton btn = (JButton) e.getSource();
+                btn.setBackground(Color.decode("#00000"));
+                btn.setForeground(Color.decode("#FFFFFF"));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                JButton btn = (JButton) e.getSource();
+                btn.setBackground(Color.decode("#FFFFFF"));
+                btn.setForeground(Color.decode("#00000"));
+            }
+        };
+
 
         frame.add(title);
         frame.add(productPanel);
         cart.add(cartLabel);
         cart.add(header);
         cart.add(cartTable);
+        decreaseButton.addMouseListener(hoverEffect);
+        clearButton.addMouseListener(hoverEffect);
+        checkoutButton.addMouseListener(hoverEffect);
         cart.add(decreaseButton);
         cart.add(clearButton);
         cart.add(checkoutButton);
--- src/ProductButton.java
+++ src/ProductButton.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.*;
@@ -8,17 +9,31 @@ class ProductButton extends JButton {
     private final String name;
     private final String price;
     private final Font titleFont;
+    private boolean hover = false;
 
     public ProductButton(String name, double price, Image image) throws IOException, FontFormatException {
         this.name = name;
         this.price = String.format("%.2f", price);
         this.image = image;
-        this.titleFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Akwe-Pro-Bold.otf")).deriveFont(12f);
+        this.titleFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/SFPRODISPLAYBOLD.OTF"));
 
         setContentAreaFilled(false);
         setBorderPainted(false);
         setFocusPainted(false);
         setOpaque(false);
+
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                hover = true;
+                repaint();
+            }
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                hover = false;
+                repaint();
+            }
+        });
     }
 
     @Override
@@ -26,28 +41,38 @@ class ProductButton extends JButton {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g2.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        g2.setColor(Color.decode("#FFFFFF"));
+        g2.fillRect(0, 0, 157, 185);
 
-        g2.setFont(titleFont);
+        if (hover) {
+            g2.setColor(Color.decode("#474747"));
+            g2.setStroke(new BasicStroke(2f));
+            g2.drawRect(0, 0, 156, 184);
+        }
 
-        int nameX = 3;
-        int nameY = 16 ;
-        g2.setColor(new Color(0, 0, 0, 200));
-        g2.drawString(name, nameX + 1, nameY + 1);
-        g2.setColor(Color.decode("#FEFCEF"));
+        int imgMargin = 10;
+        int imgWidth = 135;
+        int imgHeight = 135;
+
+        g2.setClip(new Rectangle(11, imgMargin, imgWidth, imgHeight));
+        g2.drawImage(image, 11, imgMargin, imgWidth, imgHeight, this);
+        g2.setClip(null);
+
+        g2.setFont(titleFont.deriveFont(12f));
+        FontMetrics fm = g2.getFontMetrics();
+        int nameX = (getWidth() - fm.stringWidth(name)) / 2;
+        int nameY = imgMargin + imgHeight + fm.getAscent() + 3;
+        g2.setColor(Color.decode("#000000"));
         g2.drawString(name, nameX, nameY);
 
         String priceText = price;
-        int priceX = 3;
-        int priceY = getHeight() - 6;
-
-        g2.setFont(titleFont.deriveFont(18f));
-        g2.setColor(new Color(0, 0, 0, 200));
-        g2.drawString(priceText, priceX + 1, priceY + 1);
-        g2.setColor(Color.decode("#FEFCEF"));
+        g2.setFont(titleFont.deriveFont(10f));
+        fm = g2.getFontMetrics();
+        int priceX = (getWidth() - fm.stringWidth(priceText)) / 2;
+        int priceY = nameY + fm.getHeight() + 1;
+        g2.setColor(Color.decode("#069122"));
         g2.drawString(priceText, priceX, priceY);
 
         g2.dispose();
-
     }
 }
